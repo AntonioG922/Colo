73f5e984b3524169d03bdcecf469b7b2a5d6cc4b
Anthony Gargano <agargano@phdreturns.com>
2019-02-24 13:25:57 -0500

    Add colored drink recipes

--- colo.py
+++ colo.py
@@ -1,5 +1,5 @@
 import time
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 from multiprocessing.dummy import Pool as ThreadPool
 from functools import partial
 try:
@@ -12,6 +12,7 @@ except:
 # Contains a mapping for each drink to its  1) Ingredients
 #                                           2) Image
 #                                           3) Method of serving (i.e. shaken, stirred, none)
+'''
 drinkMap = {
     "Margarita": {
         "ingredients": {
@@ -28,8 +29,91 @@ drinkMap = {
                 "unit": "oz"
             }
         },
-        "img": "",
         "serve": "shaken"
+    },
+    "Ginn fizz": {
+        "ingredients": {
+            "gin": {
+                "amount": 2,
+                "unit": "oz"
+            },
+            "simple syrup": {
+                "amount": 0.75,
+                "unit": "oz"
+            },
+            "lime-juice": {
+                "amount": 0.75,
+                "unit": "oz"
+            },
+            "Soda Water":{
+                "amount":1.5,
+                "unit": "oz"
+            }
+        },
+        "serve": "shaken"
+    },
+    "Gimlet": {
+        "ingredients": {
+            "gin": {
+                "amount": 2,
+                "unit": "oz"
+            },
+            "simple syrup": {
+                "amount": 0.75,
+                "unit": "oz"
+            },
+            "lime-juice": {
+                "amount": 0.75,
+                "unit": "oz"
+            }
+        },
+        "serve": "shaken"
+    }
+}
+'''
+drinkMap = {
+    "purple": {
+        "ingredients": {
+            "red1": {
+                "amount": 2,
+                "unit" : "oz"
+            },
+            "blue1": {
+                "amount":1,
+                "unit": "oz"
+            }
+        },
+        "serve":"shaken"
+    },
+    "orange": {
+        "ingredients": {
+            "red1": {
+                "amount": 1,
+                "unit" : "oz"
+            },
+            "yellow1": {
+                "amount":.75,
+                "unit": "oz"
+            },
+            "yellow2":{
+                "amount": 2.25,
+                "unit": "oz"
+            }
+        },
+        "serve":"shaken"
+    },
+    "green": {
+        "ingredients": {
+            "yellow1": {
+                "amount": 2,
+                "unit" : "oz"
+            },
+            "blue1": {
+                "amount": 1,
+                "unit": "oz"
+            }
+        },
+        "serve":"shaken"
     }
 }
 
@@ -43,10 +127,23 @@ unitMap = {
 # Ingredient Map
 # --------------------
 # Map of ingredients to GPIO pins (vials)
+'''
 ingredientMap = {
     "tequila": 5,
     "contreau": 9,
-    "lime-juice": 11
+    "lime-juice": 11,
+    "gin": 19,
+    "soda water": 13,
+    "simple syrup": 6,
+}
+'''
+ingredientMap = {
+    "red1": 6,
+    "blue1": 13,
+    "yellow1": 19,
+    "red2": 9,
+    "blue2": 11,
+    "yellow2": 5,
 }
 
 # Rasp Pi GPIO ports connected to relay board for pumps
@@ -66,14 +163,14 @@ def makeDrink(drinkName):
     # Make the Pool of workers
     pool = ThreadPool(len(ingredientList))
 
-    pool.map(dispenseIngredient, ingredientList)
+    pool.map(lambda x: dispenseIngredient(x, drinkName), ingredientList)
     
     # close the pool and wait for the work to finish 
     pool.close()
     pool.join()
 
     pumpAir()
-
+'''
 def setUpPumps():
     GPIO.setmode(GPIO.BCM)
 
@@ -81,24 +178,24 @@ def setUpPumps():
         GPIO.setup(pin, GPIO.IN)
         
 setUpPumps()
-
-def dispenseIngredient(ingredient):
+'''
+def dispenseIngredient(ingredient, drinkName):
     pin = getPinFromIngredient(ingredient)
     activatePump(pin)
-    time.sleep(getPumpTime(ingredient))
+    time.sleep(getPumpTime(ingredient, drinkName))
     disablePump(pin)
 
-def getPumpTime(ingredient):
-    unitTime = unitMap[drinkMap['Margarita']['ingredients'][ingredient]['unit']]
-    return drinkMap['Margarita']['ingredients'][ingredient]['amount'] * unitTime
+def getPumpTime(ingredient, drinkName):
+    unitTime = unitMap[drinkMap[drinkName]['ingredients'][ingredient]['unit']]
+    return drinkMap[drinkName]['ingredients'][ingredient]['amount'] * unitTime
 
 def activatePump(pin):
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
+    #GPIO.setup(pin, GPIO.OUT)
+    #GPIO.output(pin, GPIO.LOW)
     print('Activating pin ' + str(pin) + '...')
 
 def disablePump(pin):
-    GPIO.setup(pin, GPIO.IN)
+    #GPIO.setup(pin, GPIO.IN)
     print('Disabling pin ' + str(pin) + '...')
 
 def getPinFromIngredient(ingredient):
@@ -128,7 +225,7 @@ window.maxsize(width=800, height=480)
 
 # Create frames to organize/separate between logo and prompt and drink option buttons.
 topFrame = Frame(window).pack()
-bottomFrame = Frame(window).pack(side="bottom")
+bottomFrame = Frame(window).pack()
 
 # Add colo Logo to Window Top Frame.
 Logo = PhotoImage(file = "colo_Logo.png")
@@ -136,8 +233,11 @@ Logo = PhotoImage(file = "colo_Logo.png")
 Label(topFrame, image = Logo, padx = 0, pady = 0, borderwidth = 0, highlightthickness = 0).pack()
 
 # Add buttons for drink options. Upon clicking,
-for drink in drinkMap:
-    photo = PhotoImage(file = 'drinks/' + drink.lower() + '.png')
-    Button(bottomFrame, text = drink, image = photo, command = partial(makeDrink, drink), compound = "top").pack(pady = 5)
+orangePhoto = PhotoImage(file = 'drinks/orange.png')
+Button(bottomFrame, image = orangePhoto, text = "Orange", command = partial(makeDrink, "Orange"), compound = "top").pack(padx = 80, side="left")
+greenPhoto = PhotoImage(file = 'drinks/green.png')
+Button(bottomFrame, image = greenPhoto, text = "Green", command = partial(makeDrink, "Green"), compound = "top").pack(padx = 80, side="left")
+purplePhoto = PhotoImage(file = 'drinks/purple.png')
+Button(bottomFrame, image = purplePhoto, text = "Purple", command = partial(makeDrink, "Purple"), compound = "top").pack(padx = 80, side="left")
 
 window.mainloop()
