bd151e595e1cbd7737758cd065bdf89c6f912e64
Aniket Palkar <aniketp97@Anikets-MacBook-Pro-2.local>
2019-04-03 22:11:17 -0400

    testing ramping function

--- shaker.py
+++ shaker.py
@@ -41,7 +41,8 @@ sleep(.01)
 
 GPIO.output(DIR, CCW)
 for x in range(1,step_count):
-    delay = 0.005/(x**(1/2)) # should start slow and ramp up speed
+    mod = x/10
+    delay = 0.005/(mod+1) # should start slow and ramp up speed
     GPIO.output(STEP, GPIO.HIGH)
     sleep(delay)
     GPIO.output(STEP, GPIO.LOW)
