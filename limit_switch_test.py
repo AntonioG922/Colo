065deea81ba928ed46d0a18f4938c7c4e100f863
Aniket Palkar <aniketp97@Anikets-MacBook-Pro-2.local>
2019-04-11 20:43:43 -0400

    test to see how the switch works

---
+++ limit_switch_test.py
@@ -0,0 +1,17 @@
+#Limit Switch Test
+#Created 4/11/19
+#Updated 4/11/19
+
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #pin 15 is connected to the switch
+
+while True: # Setup a while loop to wait for a button press
+    if(GPIO.input(15)): # Setup an if loop to run a shutdown command when button press sensed
+        print('1')
+    else:
+        print('0')
+    break
+    time.sleep(1)
