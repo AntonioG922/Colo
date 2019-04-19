2f8878b3a9f7f155f7126028b3acba9a7f8e8b0f
Anthony Gargano <agargano@phdreturns.com>
2019-04-19 09:22:25 -0400

    Add front end api calls

--- site/static/index.js
+++ site/static/index.js
@@ -10,21 +10,93 @@ $(document).ready( function() {
         $('#drinkName').html(drinkName);
     });
 
-    $('#regularBtn, #strongBtn').click(function() {
-        $('.circle, #drinkProgress').show();
-        $('#drinkImg, #regularBtn, #strongBtn, .close').hide();
-        $('#drinkProgress').html('Pouring drink...');
-        strokeTransition();
-        increaseNumber();
-
-        setTimeout(() => $('#drinkProgress').html('Mixing drink...'), 5000);
-        setTimeout(() => $('#drinkProgress').html('Dispensing drink...'), 8000);
-        setTimeout(() => $('#drinkProgress').html('Drink Complete!'), 10000);
-        setTimeout(() => $('.close').show(), 10000);
+    $('#regularBtn').click(function() {
+        showDrinkProgress('regular');
     });
 
+    $('#strongBtn').click(function() {
+        showDrinkProgress('strong');
+    });
 
-})
+    $('#saveSettingsBtn').click(function() {
+        updateVials();
+    });
+    
+    $('#addDrinkBtn').click(function() {
+        addDrink();
+    });
+});
+
+function showDrinkProgress(strength) {
+    $('.circle, #drinkProgress').show();
+    $('#drinkImg, #regularBtn, #strongBtn, .close').hide();
+    $('#drinkProgress').html('Pouring drink...');
+
+    drink = $('#drinkName').html();
+    sendDrinkOrder(drink, strength);
+
+    setTimeout(() => $('#drinkProgress').html('Mixing drink...'), 5000);
+    setTimeout(() => $('#drinkProgress').html('Dispensing drink...'), 8000);
+    setTimeout(() => $('#drinkProgress').html('Drink Complete!'), 10000);
+    setTimeout(() => $('.close').show(), 10000);
+}
+
+function sendDrinkOrder(drink, strength) {
+    $.ajax({
+        url: '/api/makeDrink/' + drink + '/' + strength
+    }).done(function(data) {
+        strokeTransition();
+        increaseNumber();
+    });
+}
+
+function updateVials() {
+    vials = {
+        vial1: $('#vial1Input').val(),
+        vial2: $('#vial2Input').val(),
+        vial3: $('#vial3Input').val(),
+        vial4: $('#vial4Input').val(),
+        vial5: $('#vial5Input').val(),
+        vial6: $('#vial6Input').val()
+    }
+    
+    $.ajax({
+        url: '/api/updateVials',
+        data: vials
+    }).done(function(data) {
+        alert('Vials Updated!');
+    });
+}
+
+function addDrink() {
+    ingredients = {};
+    ingredients[$('#drinkIngredient1Input').val()] = {
+        amount: $('#drinkAmount1Input').val(),
+        unit: $('#drinkUnit1Input').val().toLowerCase()
+    };
+    ingredients[$('#drinkIngredient2Input').val()] = {
+        amount: $('#drinkAmount2Input').val(),
+        unit: $('#drinkUnit2Input').val().toLowerCase()
+    };
+    ingredients[$('#drinkIngredient3Input').val()] = {
+        amount: $('#drinkAmount3Input').val(),
+        unit: $('#drinkUnit3Input').val().toLowerCase()
+    };
+    
+    drink = {
+        name: $('#drinkNameInput').val(),
+        ingredients: ingredients,
+        served: $('#drinkServedInput').val().toLowerCase()
+    }
+    
+    
+    $.ajax({
+        url: '/api/addDrink',
+        data: drink
+    }).done(function(data) {
+        alert('Drink Added!');
+    });
+}
 
 function strokeTransition() {
     const transitionDuration = 10000;
