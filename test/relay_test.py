1e5f076745d97a79a4295363cc8e4631179a2dee
Aniket Palkar <aniketp97@Anikets-MacBook-Pro-2.local>
2019-04-21 22:43:49 -0400

    see which one is which

--- test/relay_test.py
+++ test/relay_test.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 gpioList = [26, 19, 13, 6, 5, 11, 9, 10]   
-t = 3          #Sleep Time
+t = 5          #Sleep Time
 
 for pin in gpioList:
     GPIO.setup(pin, GPIO.OUT)
